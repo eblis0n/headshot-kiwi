b6e161f042ee0daaf50cf5a412c70b91c8c2d5bf
eblis0n <eblis0n@hotmail.com>
2025-05-22 16:50:52 +0800

    1

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @version: 1.0.0
  * @Author: Eblis
  * @Date: 2025-05-21 14:34:09
- * @LastEditTime: 2025-05-22 15:54:22
+ * @LastEditTime: 2025-05-22 16:47:18
  */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -11,8 +11,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  distDir: '.next',
-  assetPrefix: '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : undefined,
   images: {
     domains: ['assets.headshotkiwi.com'],
     unoptimized: true,
